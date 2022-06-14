948b7e25d8e86f0383d11c83508fcc2ac4773e51
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-06-14 14:29:06 -0300

    Add files via upload

--- modelo.py
+++ modelo.py
@@ -1,21 +1,53 @@
+class Programa:
+        def __init__(self, nome, ano):
+            self._nome = nome.title()
+            self.ano = ano
+            self._likes = 0
 
-class Filme:
+        @property
+        def likes(self):
+            return self._likes
+
+        def curtida(self):
+            self._likes += 1
+
+        @property
+        def nome(self):
+            return self._nome
+
+        @nome.setter
+        def nome(self, novo_nome):
+            self._nome = novo_nome.title()
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.nome= nome
-        self.ano= ano
-        self.duracao= duracao
-
-topgun2 = Filme("Top Gun: Maverick", 2022, 137)
-
-print("Nome: {} - Ano: {} - Duração: {} Min".format(topgun2.nome,topgun2.ano, topgun2.duracao))
+        
+        self.duracao = duracao
 
 
-class Serie:
-    def __init__(self,nome,ano,temporadas):
-        self.nome = nome
+
+class Serie(Programa):
+    def __init__(self,nome ,ano ,temporadas):
+        self._nome = nome.title()
         self.ano = ano
+        self._likes = 0
         self.temporadas = temporadas
 
-twd = Serie("The Walking Dead", 2010,11 )
 
-print("Nome: {} - Ano: {} - Temporadas: {}".format(twd.nome,twd.ano, twd.temporadas))
+
+twd = Serie("the walking dead", 2010,11,)
+
+twd.curtida()
+twd.curtida()
+twd.curtida()
+twd.curtida()
+
+print("Nome: {} - Ano: {} - Temporadas: {} - Curtidas {}".format(twd.nome,twd.ano, twd.temporadas,twd.likes))
+
+topgun2 = Filme("top gun: maverick", 2022, 137, )
+topgun2.curtida()
+topgun2.curtida()
+topgun2.curtida()
+
+
+print("Nome: {} - Ano: {} - Duração: {} Min - Curtidas {}".format(topgun2.nome,topgun2.ano, topgun2.duracao,topgun2.likes))
